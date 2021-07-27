a56e5ab6b2b7fe9710cd748cb0a6db1afaeb9456
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2021-07-28 03:38:35 +0900

    Low-level Typography component is added

--- components/home/Article/Article.tsx
+++ components/home/Article/Article.tsx
@@ -1,15 +1,25 @@
 import cn from 'classnames';
 import React from 'react';
 import Button from '../../ui/Button/Button';
+import Typography from '../../ui/Typography/Typography';
 
 import s from './Article.module.scss';
 
 const Article = () => {
   return (
     <article className={cn(s.article)}>
+      <Typography as="span" className={s.prologue}>
+        Prologue
+      </Typography>
+
       <section className={s.section}>
-        <h2 className={s.title}>Section Title</h2>
-        <p className={s.description}>Why are you so serious??</p>
+        <Typography as="h2" className={s.title}>
+          Section Title
+        </Typography>
+
+        <Typography as="p" className={s.description}>
+          Why are you so serious??
+        </Typography>
 
         <div className={s.buttonContainer}>
           <Button>Button</Button>
---
+++ components/ui/Typography/Typography.tsx
@@ -0,0 +1,24 @@
+import { ReactNode, forwardRef, createElement } from 'react';
+import cn from 'classnames';
+
+type TypographyProps = JSX.IntrinsicElements['p'] & {
+  readonly children: ReactNode;
+  readonly as: string;
+  readonly className: string;
+};
+
+const Typography = forwardRef<HTMLElement, Partial<TypographyProps>>(
+  (props, typoRef) => {
+    const { as = 'p', children, className, ...rest } = props;
+
+    return createElement(
+      as,
+      { className: cn(className), ref: typoRef, ...rest },
+      children
+    );
+  }
+);
+
+Typography.displayName = 'Typography';
+
+export default Typography;
